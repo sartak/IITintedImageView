9ccad93f228d29573513ca031d1796b72ca9a81a
Shawn M Moore <code@sartak.org>
2013-05-21 10:51:16 -0400

    IITintedImageView first cut

---
+++ IITintedImageView.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+@interface IITintedImageView : UIView
+
+@property (nonatomic, strong) UIImage *image;
+@property (nonatomic, strong) UIColor *tintColor UI_APPEARANCE_SELECTOR;
+
+-(id)initWithImage:(UIImage *)image tinted:(UIColor *)tintColor;
+
+@end
---
+++ IITintedImageView.m
@@ -0,0 +1,57 @@
+#import "IITintedImageView.h"
+
+@implementation IITintedImageView
+
+-(id)init {
+    self = [super init];
+    if (self) {
+        self.backgroundColor = [UIColor clearColor];
+        self.opaque = NO;
+    }
+    return self;
+}
+
+-(id)initWithImage:(UIImage *)image tinted:(UIColor *)tintColor {
+    self = [self init];
+    if (self) {
+        self.image = image;
+        self.tintColor = tintColor;
+    }
+    return self;
+}
+
+-(void) setImage:(UIImage *)image {
+    _image = image;
+    self.bounds = CGRectMake(0, 0, image.size.width, image.size.height);
+    [self setNeedsDisplay];
+}
+
+-(void) setTintColor:(UIColor *)tintColor {
+    _tintColor = tintColor;
+    [self setNeedsDisplay];
+}
+
+- (void)drawRect:(CGRect)rect {
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
+    //resolve CG/iOS coordinate mismatch
+    CGContextScaleCTM(context, 1, -1);
+    CGContextTranslateCTM(context, 0, -rect.size.height);
+
+    //set the clipping area to the image
+    CGContextClipToMask(context, rect, self.image.CGImage);
+
+    if (self.tintColor) {
+        //set the fill color
+        CGContextSetFillColor(context, CGColorGetComponents(self.tintColor.CGColor));
+        CGContextFillRect(context, rect);
+
+        //blend mode overlay
+        CGContextSetBlendMode(context, kCGBlendModeDestinationAtop);
+    }
+
+    //draw the image
+    CGContextDrawImage(context, rect, self.image.CGImage);
+}
+
+@end
